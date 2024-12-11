16f35de34bad074197cb72e54bb203e6842bda0b
Samuel Avella <samu.av.jm18@gmail.com>
2024-12-11 14:13:36 +0100

    Ultimos ej

---
+++ Ejercicio4/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio4
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Mostrar el mensaje en un cuadro de mensaje (MessageBox)
+            MessageBox.Show("¡Has hecho clic en el botón!");
+        }
+    }
+}
---
+++ Ejercicio5/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio5
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClickEvent(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Evento Button.Click - Un evento de acción directa");
+        }
+
+        // Evento para el botón de MouseEnter
+        private void OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Evento MouseEnter - Detecta cuando el ratón entra en el botón");
+        }
+
+        // Evento para el clic derecho (PreviewMouseRightButtonDown)
+        private void OnRightClickEvent(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Evento Button_MouseRightButtonDown - Un evento de clic específico con el botón derecho");
+        }
+    }
+}
